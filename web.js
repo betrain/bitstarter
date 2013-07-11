2d97432b655194474c15cfba52dd2f94ff5adffd
Bharath Narayanan <betrain@gmail.com>
2013-07-11 18:35:22 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var fs = require('fs');
   var results =  new Buffer(fs.readFileSync('index.html'), 'utf-8');
   response.send(results.toString());
   
